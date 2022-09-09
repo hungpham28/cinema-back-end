a396613372bf92fd2d5558288c927adf3497ed4a
hungpham28 <88095612+hungpham28@users.noreply.github.com>
2022-09-09 23:06:24 +0700

    Update ClientURL.java

--- src/main/java/com/example/cinema_back_end/constants/ClientURL.java
+++ src/main/java/com/example/cinema_back_end/constants/ClientURL.java
@@ -1,5 +1,5 @@
 package com.example.cinema_back_end.constants;
 
 public class ClientURL {
-	public static String baseURL="http://localhost:8081";
+	public static String baseURL="https://ap-movie-cinema.herokuapp.com";
 }
