973d6c99a37f8b24fdfcdb9292659fa1563368b3
hungpham28 <88095612+hungpham28@users.noreply.github.com>
2022-09-10 11:03:34 +0700

    Update CinemaBackEndApplication.java

--- src/main/java/com/example/cinema_back_end/CinemaBackEndApplication.java
+++ src/main/java/com/example/cinema_back_end/CinemaBackEndApplication.java
@@ -32,7 +32,7 @@ public class CinemaBackEndApplication {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:8081")
+                registry.addMapping("/**").allowedOrigins("*")
                 .allowedHeaders("*").allowedMethods("*");
             }
         };
